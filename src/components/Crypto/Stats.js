a4d02d42327d7ac8b5e43e8086b7f7917ff82502
Anindya De <anindyade64@gmail.com>
2021-06-21 00:55:36 -0600

    crypto update 1

--- src/components/Crypto/Stats.js
+++ src/components/Crypto/Stats.js
@@ -12,16 +12,22 @@ function Stats() {
           <th>Total Price ($USD)</th>
         </tr>
         <tr>
-          <td>Binance Coin, BNB</td>
-          <td>390.62</td>
-          <td>8.37</td>
-          <td>3269</td>
-        </tr>
-        <tr>
           <td>Cardano, ADA</td>
           <td>1.50</td>
           <td>1977.52</td>
           <td>2966</td>
+        </tr>
+        <tr>
+          <td>Bitcoin, BTC</td>
+          <td>35595</td>
+          <td>0.08</td>
+          <td>2795</td>
+        </tr>
+        <tr>
+          <td>Ethereum, Eth</td>
+          <td>0.99</td>
+          <td>2002</td>
+          <td>1982</td>
         </tr>
         <tr>
           <td>Chainlink, LINK</td>
@@ -34,7 +40,7 @@ function Stats() {
         See current value of coins at
         <a href="https://coinmarketcap.com/" target="_blank" rel="noreferrer">Coin Marketcap</a>.
         All coins purchased are in the top 20 marketcap of all crypto,
-        so not much scrolling needed to see my losses :)
+        {' '} so not much scrolling needed to see my losses :)
       </p>
     </div>
   );
